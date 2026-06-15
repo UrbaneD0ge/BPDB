9cce5f591ddf4330bf644e4758eaa017d49b4775
Kip <Kipling.Dunlap@gmail.com>
2026-06-14 23:38:35 -0400

    remove empty style block from signin

--- src/routes/auth/signin/+page.svelte
+++ src/routes/auth/signin/+page.svelte
@@ -1,21 +1,21 @@
 <h1>Sign In</h1>
 <p>Use the form below to sign in to your account.</p>
 
-<form method="POST">
-    <label for="email">Email:</label><br>
-    <input type="email" id="email" name="email" required><br><br>
+    <form method="POST">
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br><br>
 
-    <label for="password">Password:</label><br>
-    <input type="password" id="password" name="password" required><br><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
 
-    <button type="submit" value="Sign In">Sign In</button>
-<br><br>
-    <p class="my-2">Don't have an account? <a href="/auth/signup" class="underline">Sign up here</a>.</p>
+        <button type="submit" value="Sign In">Sign In</button>
+    <br><br>
+        <p class="my-2">Don't have an account? <a href="/auth/signup" class="underline">Sign up here</a>.</p>
 
-    <!-- TODO: Get form messages/errors! -->
-</form>
+        <!-- TODO: Get form messages/errors! -->
+    </form>
 
-<style lang="postcss">
+<!-- <style lang="postcss">
 @reference "tailwindcss"
 
-</style>
+</style> -->
