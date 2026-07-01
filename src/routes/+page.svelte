0e2a7cf87c979b93defae47f57af936c752b9e2f
Kip <Kipling.Dunlap@gmail.com>
2026-06-30 23:25:10 -0400

    optional chaining on overall rating for submitted but not rated nuts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,7 @@ let mapBounds = $derived.by(() => {
 
     <div class="flex flex-row border border-black rounded-lg bg-gray-600/90 text-white p-4 mb-4 shadow-md gap-4">
       <div class="flex flex-col items-center justify-center w-30">
-        <h3 class="text-3xl font-rounded-extrabold">{peanut.avg_overall.toFixed(2) || '-'}</h3><h4>{star.repeat(parseInt(peanut.avg_overall))}</h4>
+        <h3 class="text-3xl font-rounded-extrabold">{peanut?.avg_overall?.toFixed(2) || '-'}</h3><h4>{star.repeat(parseInt(peanut.avg_overall))}</h4>
       </div>
 
       <div>
